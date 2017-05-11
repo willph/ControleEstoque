ef7a0782c24a8f1ed7c8d4bd6aeff5dd4568fa4d
Wilmerson Felipe <will.phelipe@gmail.com>
2017-05-11 20:41:35 -0300

    Projeto Java Unibratec

---
+++ src/controleestoque/Conexao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleestoque;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author aluno
+ */
+public class Conexao {
+    private Statement stmt;
+    private Connection conn;
+    
+     public Statement conectar() throws ClassNotFoundException, SQLException {
+        try {
+
+            String driver = "com.mysql.jdbc.Driver";
+            String dataBaseName = "merciaria";
+            String url = "jdbc:mysql://localhost:3306/";
+            String usuario = "root";
+            String senha = "";
+            Class.forName(driver).newInstance();
+            conn = (Connection) DriverManager.getConnection(url + dataBaseName, usuario, senha);
+            stmt = conn.createStatement();
+            return stmt;
+        } catch (InstantiationException ex) {
+            throw new SQLException(ex.getMessage());
+        } catch (IllegalAccessException ex) {
+            throw new SQLException(ex.getMessage());
+        }
+    }
+      public void desconectar() throws SQLException {
+        conn.close();
+    }
+    
+}
